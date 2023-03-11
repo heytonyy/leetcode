899fdaad5bea0a9c7bb71df9dfcc127f1c4e0c8f
Tony Aiello <heytonyyy3@gmail.com>
2023-03-10 20:18:21 -0800

    update

--- Python/final_price_w_discount.py
+++ Python/final_price_w_discount.py
@@ -3,4 +3,14 @@ from typing import List
 
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        pass
+        output=[]
+
+        for i in range(len(prices)):
+            for j in range(1,len(prices)):
+                if j>i and prices[j]<=prices[i]:
+                    output.append(prices[i]-prices[j])
+                    break
+            else:
+                output.append(prices[i])
+                
+        return output
